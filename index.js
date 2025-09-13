7e8b280b29058ef9c1333fa04a0f82f997124cc3
weeqeecombe50 <mynieagbona@hotmail.com>
2025-09-13 18:04:03 +0300

    Updated welcome message for clarity and engagement.

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Welcome to the Version Control and Management System! We hope you enjoy your experience using this application.');
+  res.send('Welcome to the Version Control and Management System! We hope you have a great experience using this application.');
 });
 
 app.listen(PORT, () => {
